5001504d764d31970bfe6798ad7ed470fd388bca
ADMIN <tranthanhtung110699@gmail.com>
2021-12-30 13:59:09 +0700

    Updates

---
+++ precache-manifest.2ed6c35927dc804c0c9cecca36ca9eaa.js
@@ -0,0 +1,194 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5529445b4e7150aee55393d604c9fc9",
+    "url": "/rdos/index.html"
+  },
+  {
+    "revision": "d6bd77bbc422e38280bb",
+    "url": "/rdos/static/css/2.1dcf17fc.chunk.css"
+  },
+  {
+    "revision": "1bbbe71847e3fdf32e69",
+    "url": "/rdos/static/css/main.f82796e6.chunk.css"
+  },
+  {
+    "revision": "d6bd77bbc422e38280bb",
+    "url": "/rdos/static/js/2.05cb39c6.chunk.js"
+  },
+  {
+    "revision": "8f452ecf1affe93fc606eac6c17c62e5",
+    "url": "/rdos/static/js/2.05cb39c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f139f127a3bc4262fb1",
+    "url": "/rdos/static/js/3.2e4f0581.chunk.js"
+  },
+  {
+    "revision": "c15eddeb09b46511cf0731116c7067b0",
+    "url": "/rdos/static/js/3.2e4f0581.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1bbbe71847e3fdf32e69",
+    "url": "/rdos/static/js/main.53d9c0f0.chunk.js"
+  },
+  {
+    "revision": "73b14b77e0efdeac946b",
+    "url": "/rdos/static/js/runtime-main.b11fdad2.js"
+  },
+  {
+    "revision": "ebe3c7cd7dfe47ca9ea52160943fc126",
+    "url": "/rdos/static/media/applepay.ebe3c7cd.mp3"
+  },
+  {
+    "revision": "b5c826fc619edc4cc35993d0cd3c827c",
+    "url": "/rdos/static/media/bg-ico-hero.b5c826fc.jpg"
+  },
+  {
+    "revision": "06ab33ae3e2cbae0f509cd05f2ee6793",
+    "url": "/rdos/static/media/boxicons.06ab33ae.eot"
+  },
+  {
+    "revision": "5bbebd73a37de0cc242e872b4483269e",
+    "url": "/rdos/static/media/boxicons.5bbebd73.woff"
+  },
+  {
+    "revision": "66d9fae4a22d74c2618f44c585a0c1a8",
+    "url": "/rdos/static/media/boxicons.66d9fae4.svg"
+  },
+  {
+    "revision": "72589e162de78952d4d6b5ce8b832175",
+    "url": "/rdos/static/media/boxicons.72589e16.ttf"
+  },
+  {
+    "revision": "aeaf4ee6e2c712532f982c8a4dd68b16",
+    "url": "/rdos/static/media/boxicons.aeaf4ee6.woff2"
+  },
+  {
+    "revision": "b08fa5629bc82d58da51a2a4c7be51d5",
+    "url": "/rdos/static/media/ding-sound-effect_2.b08fa562.mp3"
+  },
+  {
+    "revision": "303a94972c4a0f13290e6dcf3660af95",
+    "url": "/rdos/static/media/discord-notification.303a9497.mp3"
+  },
+  {
+    "revision": "0253a9a5e15b217b18ade085c5390ef5",
+    "url": "/rdos/static/media/dripicons-v2.0253a9a5.svg"
+  },
+  {
+    "revision": "11fc83ae11617015f2fcde2065fb34d3",
+    "url": "/rdos/static/media/dripicons-v2.11fc83ae.woff"
+  },
+  {
+    "revision": "7e12564e72400735ae5b671780f6a9f7",
+    "url": "/rdos/static/media/dripicons-v2.7e12564e.eot"
+  },
+  {
+    "revision": "cf09c981aeaa8736810133ab1148e4a3",
+    "url": "/rdos/static/media/dripicons-v2.cf09c981.ttf"
+  },
+  {
+    "revision": "c7ae99ce871c7f754a0271483f7d136f",
+    "url": "/rdos/static/media/error-img.c7ae99ce.png"
+  },
+  {
+    "revision": "13685372945d816a2b474fc082fd9aaa",
+    "url": "/rdos/static/media/fa-brands-400.13685372.ttf"
+  },
+  {
+    "revision": "3debcaf1a98e4445d213fd0089d42f6b",
+    "url": "/rdos/static/media/fa-brands-400.3debcaf1.svg"
+  },
+  {
+    "revision": "a06da7f0950f9dd366fc9db9d56d618a",
+    "url": "/rdos/static/media/fa-brands-400.a06da7f0.woff2"
+  },
+  {
+    "revision": "c1868c9545d2de1cf8488f1dadd8c9d0",
+    "url": "/rdos/static/media/fa-brands-400.c1868c95.eot"
+  },
+  {
+    "revision": "ec3cfddedb8bebd2d7a3fdf511f7c1cc",
+    "url": "/rdos/static/media/fa-brands-400.ec3cfdde.woff"
+  },
+  {
+    "revision": "261d666b0147c6c5cda07265f98b8f8c",
+    "url": "/rdos/static/media/fa-regular-400.261d666b.eot"
+  },
+  {
+    "revision": "89f9806b964f92ad282fd60947eba588",
+    "url": "/rdos/static/media/fa-regular-400.89f9806b.svg"
+  },
+  {
+    "revision": "c20b5b7362d8d7bb7eddf94344ace33e",
+    "url": "/rdos/static/media/fa-regular-400.c20b5b73.woff2"
+  },
+  {
+    "revision": "db78b9359171f24936b16d84f63af378",
+    "url": "/rdos/static/media/fa-regular-400.db78b935.ttf"
+  },
+  {
+    "revision": "f89ea91ecd1ca2db7e09baa2c4b156d1",
+    "url": "/rdos/static/media/fa-regular-400.f89ea91e.woff"
+  },
+  {
+    "revision": "1ab236ed440ee51810c56bd16628aef0",
+    "url": "/rdos/static/media/fa-solid-900.1ab236ed.ttf"
+  },
+  {
+    "revision": "a0369ea57eb6d3843d6474c035111f29",
+    "url": "/rdos/static/media/fa-solid-900.a0369ea5.eot"
+  },
+  {
+    "revision": "ad912fd102f4052d3273dd838e0c671d",
+    "url": "/rdos/static/media/fa-solid-900.ad912fd1.svg"
+  },
+  {
+    "revision": "b15db15f746f29ffa02638cb455b8ec0",
+    "url": "/rdos/static/media/fa-solid-900.b15db15f.woff2"
+  },
+  {
+    "revision": "bea989e82b07e9687c26fc58a4805021",
+    "url": "/rdos/static/media/fa-solid-900.bea989e8.woff"
+  },
+  {
+    "revision": "15424912ffabe07dbb6f95944a8af3ef",
+    "url": "/rdos/static/media/incorrect.swf.15424912.mp3"
+  },
+  {
+    "revision": "645af864b3c2019cf428c4c4b5a48680",
+    "url": "/rdos/static/media/ka-ching.645af864.mp3"
+  },
+  {
+    "revision": "0ba9d005c4db40d30aac31d6c6f901d4",
+    "url": "/rdos/static/media/logo-after-design.0ba9d005.png"
+  },
+  {
+    "revision": "5be3ef82ddef202940bacce662befeb2",
+    "url": "/rdos/static/media/logo-web.5be3ef82.jpg"
+  },
+  {
+    "revision": "f7686e584d8e57b8c640a110d3597246",
+    "url": "/rdos/static/media/logo.f7686e58.svg"
+  },
+  {
+    "revision": "63d2a5950fc212096c3612f71ae66f64",
+    "url": "/rdos/static/media/materialdesignicons-webfont.63d2a595.woff"
+  },
+  {
+    "revision": "7fb0e3780372f10f804513e2e0c39e78",
+    "url": "/rdos/static/media/materialdesignicons-webfont.7fb0e378.woff2"
+  },
+  {
+    "revision": "a1a0ed860f50ffd42ba46d7c10f30255",
+    "url": "/rdos/static/media/materialdesignicons-webfont.a1a0ed86.eot"
+  },
+  {
+    "revision": "fe1545ef4dd1eef2f1e25528898fc0b3",
+    "url": "/rdos/static/media/materialdesignicons-webfont.fe1545ef.ttf"
+  },
+  {
+    "revision": "ba4e037e1b9676c5215541748eaa1e42",
+    "url": "/rdos/static/media/profile-img.ba4e037e.png"
+  }
+]);
